541f6fbdc04e52817563dbeeeded3d3584e3cf13
lcol3117 <59921084+lcol3117@users.noreply.github.com>
2020-01-16 19:56:16 -0500

    update code to better thresholds and i2c
    added i2c and better thresholds

--- color_binary_filter_2.py
+++ color_binary_filter_2.py
@@ -4,6 +4,7 @@
 # number of thresholds to segment the image by.(19, 88, -31, 45, 9, 127)(19, 88, -31, 45, 9, 127)(19, 88, -31, 45, 9, 127)(19, 88, -31, 45, 9, 127)(19, 88, -31, 45, 9, 127)(19, 88, -31, 45, 9, 127)(19, 88, -31, 45, 9, 127)(19, 88, -31, 45, 9, 127)(19, 88, -31, 45, 9, 127)(19, 88, -31, 45, 9, 127)(19, 88, -31, 45, 9, 127)(19, 88, -31, 45, 9, 127)(19, 88, -31, 45, 9, 127)(19, 88, -31, 45, 9, 127)(19, 88, -31, 45, 9, 127)(19, 88, -31, 45, 9, 127)(19, 88, -31, 45, 9, 127)(19, 88, -31, 45, 9, 127)(19, 88, -31, 45, 9, 127)(19, 88, -31, 45, 9, 127)(19, 88, -31, 45, 9, 127)(19, 88, -31, 45, 9, 127)(19, 88, -31, 45, 9, 127)(19, 88, -31, 45, 9, 127)(19, 88, -31, 45, 9, 127)(19, 88, -31, 45, 9, 127)(19, 88, -31, 45, 9, 127)(19, 88, -31, 45, 9, 127)(19, 88, -31, 45, 9, 127)(19, 88, -31, 45, 9, 127)(19, 88, -31, 45, 9, 127)(19, 88, -31, 45, 9, 127)(19, 88, -31, 45, 9, 127)(19, 88, -31, 45, 9, 127)(19, 88, -31, 45, 9, 127)(19, 88, -31, 45, 9, 127)(19, 88, -31, 45, 9, 127)(19, 88, -31, 45, 9, 127)(19, 88, -31, 45, 9, 127)(19, 88, -31, 45, 9, 127)(19, 88, -31, 45, 9, 127)(19, 88, -31, 45, 9, 127)
 
 import sensor, image, time, pyb
+from pyb import *
 
 sensor.reset()
 sensor.set_framesize(sensor.QQVGA)
@@ -17,8 +18,13 @@ grnled = pyb.LED(2)
 torled = 0
 togled = 0
 
+allbits = 0
+
+i2c = I2C(2)                         # create on bus 2
+i2c.init(I2C.SLAVE, addr=0x3A)       # init as a slave with given address
+
 # Use the Tools -> Machine Vision -> Threshold Edtor to pick better thresholds.
-red_threshold = (0, 100, -30, 22, 28, 127) # L A B
+red_threshold = (0, 100, -18, 31, 11, 127) # L A B
 
 while(True):
 
@@ -27,7 +33,7 @@ while(True):
     img = sensor.snapshot()
     img.binary([red_threshold])
 
-    img.erode(8,8)
+    img.erode(10,10)
 
     cwmax = 0
     blobs = img.find_blobs([(100,255),(100,255),(100,255)])
@@ -41,7 +47,7 @@ while(True):
             cmax=c
         print(cmax)
     try:
-        if(abs(cmax.w()-cmax.h()) > (cmax.w()/8)):
+        if(abs(cmax.w()-cmax.h()) > (cmax.w()/4)):
             cmax = -2
     except:
         pass
@@ -53,9 +59,11 @@ while(True):
         else:
             torled = False
             togled = True
+        allbits = cmax.cx()
     except:
         torled = False
         togled = False
+        allbits = 511
     if(torled):
         redled.on()
     else:
@@ -64,4 +72,8 @@ while(True):
         grnled.on()
     else:
         grnled.off()
+    try:
+        i2c.send(allbits, timeout=5)
+    except:
+        pass
     print(clock.fps())
