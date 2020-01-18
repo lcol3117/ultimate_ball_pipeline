95e091f9724560d27c7cdd44044115985e40a31f
lcol3117 <59921084+lcol3117@users.noreply.github.com>
2020-01-18 10:40:13 -0500

    Move aspect ratio test
    Moved to above largest blob selection, so it selects largest ok blob, not largest blob if it is ok

--- color_binary_filter_2.py
+++ color_binary_filter_2.py
@@ -24,46 +24,56 @@ i2c = I2C(2)                         # create on bus 2
 i2c.init(I2C.SLAVE, addr=0x3A)       # init as a slave with given address
 
 # Use the Tools -> Machine Vision -> Threshold Edtor to pick better thresholds.
-red_threshold = (0, 100, -18, 31, 11, 127) # L A B
+red_threshold = (0, 100, -128, 7, 35, 127) # L A B threshold
 
 while(True):
 
     # Test red threshold
     clock.tick()
     img = sensor.snapshot()
-    img.binary([red_threshold])
+    img.binary([red_threshold]) #Threshold it
 
-    img.erode(10,10)
+    img.erode(5,4) #Remove noise
+    img.dilate(2,2) #Fill in logo
+    img.erode(2,2) #Account for dilation
 
     cwmax = 0
     blobs = img.find_blobs([(100,255),(100,255),(100,255)])
+    aspect_ratio_ok = blobs[:]
+    for i in range(0,len(blobs)):
+        if(abs(blobs[i].w()-blobs[i].h()) > (blobs[i].w()/4)): #Aspect Ratio Test
+            aspect_ratio_ok[i] = 0
+        else:
+            aspect_ratio_ok[i] = 1
     try:
         cmax = blobs[0]
     except:
         cmax = -1
-    for c in blobs:
-        if (int(c.w()) > int(cwmax)):
-            cwmax=c.w()
-            cmax=c
+    for c in range(0,len(blobs)):
+        if (aspect_ratio_ok[c] == 1): #Only look through good aspect ratios
+            if (int(blobs[c].w()) > int(cwmax)): #Target largest object with ok aspect ratio
+                cwmax=blobs[c].w()
+                cmax=blobs[c]
         print(cmax)
     try:
-        if(abs(cmax.w()-cmax.h()) > (cmax.w()/4)):
+        if(abs(cmax.w()-cmax.h()) > (cmax.w()/4)): #Aspect Ratio Test
             cmax = -2
     except:
         pass
     try:
-        img.draw_circle(cmax.cx(), cmax.cy(), int(cmax.w()/2), color = (255, 0, 0))
-        if (cmax.cx() >= (img.width()/2)):
+        img.draw_circle(cmax.cx(), cmax.cy(), int(cmax.w()/2), color = (255, 0, 0)) #Draw circle
+        if (cmax.cx() >= (img.width()/2)): #colored led depends on side of image, to debug
             torled = True
             togled = False
         else:
             torled = False
             togled = True
-        allbits = cmax.cx()
+        allbits = cmax.cx() #Update bits
     except:
         torled = False
         togled = False
-        allbits = 511
+        allbits = 511 #Update bits with special value 11111111
+    #Update LEDs
     if(torled):
         redled.on()
     else:
@@ -73,7 +83,7 @@ while(True):
     else:
         grnled.off()
     try:
-        i2c.send(allbits, timeout=5)
+        i2c.send(allbits, timeout=5) #Send over i2c
     except:
         pass
     print(clock.fps())
